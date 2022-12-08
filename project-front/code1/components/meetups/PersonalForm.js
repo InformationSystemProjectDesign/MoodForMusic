5ce466f90e7b6085d36e87a303db065903dfb69a
shockvanessa <shockvanessa@gmail.com>
2022-12-09 01:13:58 +0800

    調整 - 個人天地RWD

--- project-front/code1/components/meetups/PersonalForm.js
+++ project-front/code1/components/meetups/PersonalForm.js
@@ -43,34 +43,35 @@ function PersonalForm() {
       {/* <link href="https://fonts.googleapis.com/css?family=Noto+Serif+TC&amp;display=swap" rel="stylesheet"/> */}
       <script src="https://apis.google.com/js/api:client.js" async defer />
       <script src="https://accounts.google.com/gsi/client" async defer/>
-      <div className="flex flex-row place-content-center">
+      
+      <div className="flex flex-col sm:flex-row place-content-center">
         {/* 我的書桌 */}
-        <div className="basis-1/2 border-solid border-2 border-black mr-10">
-          <div className="flex items-center justify-center p-12 pt-0">
+        <div className="basis-1/2 border-solid border-2 border-black mb-10 sm:mr-10">
+          <div className="flex items-center justify-center p-8 sm:p-12 pt-0">
             <div className="mx-auto w-full max-w-[550px]">
               <form action="https://formbold.com/s/FORM_ID" method="POST">
                 <label
                   htmlFor="name"
-                  className="my-10 block text-center text-3xl font-bold"
+                  className="my-10 block text-center text-2xl sm:text-3xl font-bold"
                 >
                   <p className="underline underline-offset-4 decoration-2">我的書桌</p>
                 </label>
               
                 {/* 寫信btn */}
-                <div className="mb-5">
+                <div className="mb-3 sm:mb-5">
 
                   <Link href="personal_space/SendArticle" passHref>
 
-                    <button className="w-full rounded-md bg-white transition duration-150 ease-in-out hover:border-gray-900 hover:text-gray-900 border text-gray-800 px-6 py-2 text-base hover:bg-gray-100 focus:outline-none">
+                    <button className="w-full rounded-md bg-white transition duration-150 ease-in-out hover:border-gray-900 hover:text-gray-900 border text-gray-800 px-6 py-2 text-sm sm:text-base hover:bg-gray-100 focus:outline-none">
                       寫信
                     </button>
                   </Link>
                 </div>
 
                 {/* 信件紀錄btn */}
-                <div className="mb-5">
+                <div className="mb-3 sm:mb-5">
                   <Link href="personal_space/MailRecord">
-                    <button className="w-full rounded-md bg-white transition duration-150 ease-in-out hover:border-gray-900 hover:text-gray-900 border text-gray-800 px-6 py-2 text-base hover:bg-gray-100 focus:outline-none">
+                    <button className="w-full rounded-md bg-white transition duration-150 ease-in-out hover:border-gray-900 hover:text-gray-900 border text-gray-800 px-6 py-2 text-sm sm:text-base hover:bg-gray-100 focus:outline-none">
                       信件紀錄
                     </button>
                   </Link>
@@ -82,32 +83,32 @@ function PersonalForm() {
         </div>
 
         {/* 個人檔案 */}
-        <div className="basis-1/2 border-solid border-2 border-black  ml-10">
-          <div className="flex items-center justify-center p-12 pt-0">
+        <div className="basis-1/2 border-solid border-2 border-black  mb-10 sm:mr-10">
+          <div className="flex items-center justify-center p-8 sm:p-12 pt-0">
             <div className="mx-auto w-full max-w-[550px]">
               <form action="https://formbold.com/s/FORM_ID" method="POST">
                 <label
                   htmlFor="name"
-                  className="my-10 block text-center text-3xl font-bold ;"
+                  className="my-10 block text-center text-2xl sm:text-3xl font-bold ;"
                 >
                   <p className="underline underline-offset-4 decoration-2">個人檔案</p>
                 </label>            
 
                 {/* 修改密碼btn */}
-                <div className="mb-5">
+                <div className="mb-3 sm:mb-5">
 
                   <Link href="personal_space/ChangePassword" passHref>
                     
-                    <button className="w-full rounded-md bg-white transition duration-150 ease-in-out hover:border-gray-900 hover:text-gray-900 border text-gray-800 px-6 py-2 text-base hover:bg-gray-100 focus:outline-none">
+                    <button className="w-full rounded-md bg-white transition duration-150 ease-in-out hover:border-gray-900 hover:text-gray-900 border text-gray-800 px-6 py-2 text-sm sm:text-base hover:bg-gray-100 focus:outline-none">
                       修改密碼
                     </button>
                   </Link>
                 </div>
 
                 {/* 我的情緒圖btn */}
-                <div className="mb-5">
+                <div className="mb-3 sm:mb-5">
                   <Link href="personal_space/MyChart" passHref>
-                    <button className="w-full rounded-md bg-white transition duration-150 ease-in-out hover:border-gray-900 hover:text-gray-900 border text-gray-800 px-6 py-2 text-base hover:bg-gray-100 focus:outline-none">
+                    <button className="w-full rounded-md bg-white transition duration-150 ease-in-out hover:border-gray-900 hover:text-gray-900 border text-gray-800 px-6 py-2 text-sm sm:text-base hover:bg-gray-100 focus:outline-none">
                       我的情緒圖
                     </button>
                   </Link>
